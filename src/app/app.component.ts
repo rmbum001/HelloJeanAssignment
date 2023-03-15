0e77d8c692c0445a6fb7e9c67ba3b38576597e47
richie <mbumbarichie@gmail.com>
2023-03-15 15:24:01 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+import { PostComponent } from './post/post.component';
+import{GetApiService} from './get-api.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+public data = [];
+public apiData: any;
+
+ 
+  title = 'HelloWord'
+   
+
+  firstNumber!: string;
+
+  secondNumber!: string;
+  
+  result!: number;
+  api: any;
+  AddNumber()
+  {
+     this.result = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+  }
+  constructor (private service: GetApiService){
+
+  }
+ 
+apiCall()
+{
+  this.service.apiCall().subscribe((results) => {
+    console.log('Data is returned',results);
+    
+  })
 }
+
+
+  // apiCall(){
+  //   this.service.apiCall().subscribe((this.result) =>{
+  //     console.log('This is retrieving data', this.result);
+  //     this.data = this.result.result,
+  //   })
+  // }
+   
+  
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { FormsModule } from '@angular/forms';
+import { PracticeComponent } from './practice/practice.component';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    // NavbarComponent,
+    // PostComponent,
+    // PostListComponent,
+    // PracticeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+
+  constructor(private http: HttpClient){
+
+
+    // public getJsonValue: any;
+    // public postJsonValue: any;
+    // ngOnInit(): void {
+    // }
+
+   
+  
+   } 
+      ngOnInit(): void{
+
+      }
+  
+   public getMethod(){
+       
+    this.http.get('https://api.spacexdata.com/v5/launches/latest');
+   }
+ }
+function getMethod() {
+  throw new Error('Function not implemented.');
+}
+
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
